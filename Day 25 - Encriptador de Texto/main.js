f66311bf0d05facb87e6a435fc2d8c274a431127
Rômulo Dantas <romulodtsn@gmail.com>
2024-10-15 22:48:40 -0300

    Dia 25 - Encriptador de Texto - Código Finalizado

---
+++ Day 25 - Encriptador de Texto/main.js
@@ -0,0 +1,80 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,?!'_-&@#$%*()/:<>|+= ";
+
+// Função para encriptar
+
+function encryptText(text, key) {
+    let encryptedText = "";
+
+    for (let i = 0; i < text.length; i++) {
+        const textChar = text[i];
+        const keyChar = key[i % key.length];
+
+        const textIndex = alphabet.indexOf(textChar);
+        const keyIndex = alphabet.indexOf(keyChar);
+
+        if (textIndex === -1) {
+            encryptText += textChar;
+        } else {
+            const newIndex = (textIndex + keyIndex) % alphabet.length;
+            encryptedText += alphabet[newIndex];
+        }
+    }
+
+    return encryptedText;
+}
+
+// Função para desencriptar
+function decryptText(encryptedText, key) {
+    let decryptedText = "";
+
+    for (let i = 0; i < encryptedText.length; i++) {
+        const encryptedChar = encryptedText[i];
+        const keyChar = key[i % key.length];
+
+        const encryptedIndex = alphabet.indexOf(encryptedChar);
+        const keyIndex = alphabet.indexOf(keyChar);
+
+        if (encryptedText === -1) {
+            decryptedText += encryptedChar;
+        } else {
+            let newIndex = encryptedIndex - keyIndex;
+            if (newIndex < 0) newIndex += alphabet.length;
+            decryptedText += alphabet[newIndex];
+        }
+    }
+
+    return decryptedText;
+}
+
+// Atualizar o resultado com base na operação selecionada
+
+function updateResult(isEncrypting) {
+    const text = document.getElementById("message").value;
+    const key = document.getElementById("key").value;
+
+    let result = "";
+
+    if (isEncrypting) {
+        result = encryptText(text, key);
+    } else {
+        result = decryptText(text, key);
+    }
+
+    document.getElementById("result").textContent = result;
+}
+
+// Adicionando event listeners nos botoes
+
+document.getElementById("enc-btn").addEventListener('click', function () {
+    updateResult(true);
+});
+
+document.getElementById("dec-btn").addEventListener('click', function () {
+    updateResult(false);
+});
+
+// Inicializar o resultado com o texto encriptado quando a pagina carregar
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateResult(true);
+});
